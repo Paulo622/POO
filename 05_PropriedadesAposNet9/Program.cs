d320f5fa130577b01e425a13db38f0dcc3f4e07d
Paulo Vitor <Teodoropaulo1504@gmail.com>
2025-09-12 21:42:41 -0300

    Inclusão da Classe Calendário e Alterações- Inclusões no método principal

--- 05_PropriedadesAposNet9/Program.cs
+++ 05_PropriedadesAposNet9/Program.cs
@@ -4,13 +4,107 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _05_PropriedadesAposNet9
+namespace _05_ProproedadesAposNet9 
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Conta conta = new Conta("164-73");
 
+            conta.setNome("Paulo Vitor"); // agora funciona de verdade
+
+            conta.Depositar(1000.00m);
+            conta.ImprimirSaldo();
+
+            while (true)
+            {
+                Console.WriteLine("Informe a Operação: [D]- Depositar, [S]- Sacar ou [E]- Sair");
+                string operacao = Console.ReadLine();
+
+                if (operacao.ToUpper() == "D")
+                {
+                    Console.WriteLine("Informe o valor para depósito: ");
+                    decimal valorDeposito = decimal.Parse(Console.ReadLine());
+                    conta.Depositar(valorDeposito);
+                    conta.ImprimirSaldo();
+                }
+                else if (operacao.ToUpper() == "S")
+                {
+                    Console.WriteLine("Informe o valor para saque: ");
+                    decimal valorSaque = decimal.Parse(Console.ReadLine());
+                    conta.Sacar(valorSaque);
+                    conta.ImprimirSaldo();
+                }
+                else if (operacao.ToUpper() == "E")
+                {
+                    Console.WriteLine($"Conta: {conta.getNumero()} Nome: {conta.getNome()} Saldo: {conta.Saldo:c2}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Informe apenas as letras [D] para Depositar, [S] para Sacar ou [E] para Sair");
+                }
+            }
         }
     }
-}
+
+    public class Conta
+    {
+        public string Numero { get; } // Propriedade somente leitura
+
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        private decimal saldo;
+
+        public Conta(string numero)
+        {
+            this.Numero = numero;
+        }
+
+        public decimal Saldo
+        {
+            get { return saldo; }
+        }
+
+        public string getNumero()
+        {
+            return Numero;
+        }
+
+        public string getNome()
+        {
+            return Nome;
+        }
+
+        public void setNome(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public void Depositar(decimal valor)
+        {
+            if (valor > 0)
+                saldo += valor;
+            else
+                Console.WriteLine("Valor de depósito incorreto, deve ser maior que zero!");
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor > 0 && valor <= saldo)
+                saldo -= valor;
+            else
+                Console.WriteLine("Saque inválido! Verifique o valor e o saldo disponível.");
+        }
+
+        public void ImprimirSaldo()
+        {
+            Console.WriteLine($"Saldo Atual: {saldo:c2}");
+        }
+    }
---
+++ 06_Calendario/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using _06_Calendario;
+
+namespace _09_Calendario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("Digite o mês (1..12)");
+            // int mes = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o ano:");
+            int ano = int.Parse(Console.ReadLine());
+
+
+            Calendario calendario;
+            for (int mes = 1; mes <= 12; mes++)
+            {
+               
+                calendario = new Calendario(ano, mes);
+                calendario.ImprimirCalendario();
+
+                Console.WriteLine("\n\n");
+                //Espera o usuário teclar qualquer tecla
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
