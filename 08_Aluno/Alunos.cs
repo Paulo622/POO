aa8db7fd09a3cad94485cc1735aead91884158fd
Paulo Vitor <Teodoropaulo1504@gmail.com>
2025-09-18 20:45:17 -0300

    Criação do Projeto e Atividade Aluno

---
+++ 08_Aluno/Alunos.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Aluno
+{
+    internal class Aluno
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    nome = value;
+                }
+                else
+                {
+                    Console.WriteLine("Nome inválido");
+                }
+            }
+        }
+
+        private int idade;
+        public int Idade
+        {
+            get { return idade; }
+            set
+            {
+                if (idade >= 0)
+                {
+                    idade = value;
+                }
+                else
+                {
+                    Console.WriteLine("Idade inválida");
+                }
+        }   }      
+        
+            private double nota1 { get; set; }
+            private double nota2 { get; set; }
+
+            public Aluno(string nome)
+            {
+                this.Nome = nome;
+            }   
+
+            public double Nota1
+            {
+                get { return nota1; }
+                set { nota1 = value; }
+            }
+
+            public double Nota2
+
+            {
+                get { return nota2; }
+                set { nota2 = value; }
+            }
+
+            public double Media()
+
+            {
+            
+                return (nota1 + nota2) / 2;
+            
+            }
+
+        public string Situacao()
+        {
+            return Media() >= 6 ? "Aprovado" : "Reprovado";
+        }
+
+
+    }
+   
+}
---
+++ 08_Aluno/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Aluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do aluno: ");
+            string nome = Console.ReadLine();
+            Aluno aluno = new Aluno(nome);
+
+            Console.WriteLine("Digite a Idade do aluno: ");
+            aluno.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a 1ª Nota do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a 2ª Nota do aluno: ");
+            aluno.Nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Média: " + aluno.Media());
+            
+
+            Console.WriteLine($"Nome: {aluno.Nome} Idade: {aluno.Idade} 1ªNota : {aluno.Nota1} 2ªNota : {aluno.Nota2} Media : {aluno.Media()} Situação : {aluno.Situacao()}");
+            Console.ReadKey();
+
+            return;
+        }
+       
+    }
+}
