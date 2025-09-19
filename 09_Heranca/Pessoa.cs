68cf924909d4718138c4861537faa0380d969748
Paulo Vitor <Teodoropaulo1504@gmail.com>
2025-09-18 22:20:38 -0300

    Criação de Heranças

---
+++ 09_Heranca/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Heranca
+{
+    internal abstract class Pessoa
+    {
+        public string Nome;
+        public string Telefone;
+
+        public abstract string GetDocumento();
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"\nLISTA DE PESSOAS\nNome: {this.Nome} Documento: {this.GetDocumento()} Telefone{this.Telefone}");
+        }
+    }
+}
---
+++ 09_Heranca/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Não é possível instanciar uma classe abstrata
+            //Pessoa obj = new Pessoa();
+
+            PessoaFisica pessoaFisica = new PessoaFisica();
+            pessoaFisica.Nome = "Geraldo";
+            pessoaFisica.CPF = "123.456.789-00";
+            pessoaFisica.Telefone = "(11) 99667-6049";
+            pessoaFisica.Imprimir();
+
+            PessoaJuridica pessoaJuridica = new PessoaJuridica();
+            pessoaJuridica.Nome = "Empresa LTDA";
+            pessoaJuridica.CNPJ = "01.455.678/0001-44";
+            pessoaJuridica.Telefone = "(011) 3344-5566";
+            pessoaJuridica.Imprimir();
+
+            
+
+        }
+    }
+}
