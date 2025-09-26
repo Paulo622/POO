d7d2d2f6ec7823b062c74a3fab8ad3efa2e79551
Paulo Vitor <Teodoropaulo1504@gmail.com>
2025-09-26 20:31:10 -0300

    Stack da Atividade Principal

---
+++ 13_ColecaoPilha/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_ColecaoPilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stackNumeros = new Stack<int>();
+            stackNumeros.Push(1);
+            stackNumeros.Push(2);
+            stackNumeros.Push(3);
+            stackNumeros.Push(4);
+            stackNumeros.Push(5);
+            
+            Console.WriteLine($"\n O Ùltimo número inserido foi: {stackNumeros.Peek()}");
+            
+
+        }
+    }
+}
