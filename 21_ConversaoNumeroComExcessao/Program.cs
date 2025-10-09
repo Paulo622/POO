99cd78fd3f1b3efc9b1c427a8c0d856d5e166afb
Paulo Vitor <Teodoropaulo1504@gmail.com>
2025-10-09 20:11:39 -0300

    Atvidade de Tratamento de Excecao 2 - Divisao

--- 21_ConversaoNumeroComExcessao/Program.cs
+++ 21_ConversaoNumeroComExcessao/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _21_ConversaoNumeroComExcessao
+namespace _21_ConversaoNumeroComExcecao 
 {
     internal class Program
     {
---
+++ 22_DivisaoComExcecao/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_DivisaoComExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1,numero2, resultado;
+            while (true)
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.Write("Digite o primeiro número inteiro: ");
+                    numero1 = int.Parse(Console.ReadLine());
+
+                    Console.Write("Digite o segundo número inteiro: ");
+                    numero2 = int.Parse(Console.ReadLine());
+
+                    resultado = numero1 / numero2;
+
+
+                    Console.WriteLine("\nO Resultado da Divisão é: ");
+                    Console.WriteLine(resultado);
+                    Console.ReadKey();
+                    break;
+                }
+                catch(DivideByZeroException ex)
+                {
+                    Console.WriteLine("Não é possivel dividir um numero por 0.");
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Valor inválido! Digite apenas números inteiros.");
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                }
+                finally
+                {
+                    Console.WriteLine("Operação finalizada.Obrigado por utilizar o programa!");
+                    Console.ReadKey();
+                }
+               
+            }   
+        }
+    }
+}
