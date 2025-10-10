f197677e139ad135df8f9d57e25ea06f4a4984c9
Paulo Vitor <Teodoropaulo1504@gmail.com>
2025-10-09 21:37:54 -0300

    Melhorias

--- 23_LeituraArquivoComExcecao/Program.cs
+++ 23_LeituraArquivoComExcecao/Program.cs
@@ -15,33 +15,48 @@ namespace _23_LeituraArquivoComExcecao
 
             while (true)
             {
-                Console.WriteLine("Digite um número para ler o arquivo ou -1 para sair:");
-                leitura = int.Parse(Console.ReadLine());
                 Console.Clear();
+                Console.WriteLine("Digite um número para ler o arquivo ou -1 para sair:");
+
+               
+                if (!int.TryParse(Console.ReadLine(), out leitura))
+                {
+                    Console.WriteLine("Entrada inválida! Por favor, digite um número válido.");
+                    Console.ReadKey();
+                    
+                }
+
+                if (leitura == -1)
+                {
+                    Console.WriteLine("Encerrando o programa! Obrigado por utilizar.");
+                    break;
+                }
+
                 try
                 {
-                    if (leitura == -1)
-                    {
-                        Console.WriteLine("Encerrando o programa! Obrigado por utilizar.");
-                        break;
-                    }
-
-
+                    
                     string[] linhas = System.IO.File.ReadAllLines(caminho);
                     Console.WriteLine("Conteúdo do arquivo:");
                     foreach (string linha in linhas)
                     {
                         Console.WriteLine(linha);
                     }
-
                 }
                 catch (System.IO.FileNotFoundException)
                 {
+                    
                     Console.WriteLine("Arquivo não encontrado. Por favor, verifique o caminho e tente novamente.");
-                    Console.WriteLine(caminho);
-
+                    Console.WriteLine($"Caminho atual do arquivo: {caminho}");
+                    Console.ReadKey();
                 }
-            }     
+                catch (Exception ex)
+                {
+                    
+                    Console.WriteLine("Ocorreu um erro inesperado.");
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
